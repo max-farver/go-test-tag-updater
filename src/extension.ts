9eb66be89b1fb9509f62bc69bddeeeaf78937bd2
Max Farver <mfarver99@gmail.com>
2025-02-04 11:30:19 -0600

    vscode version and remove logs

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,5 @@
 import * as vscode from "vscode";
 
-interface VSCodeSettings {
-  "go.testTags"?: string;
-  [key: string]: unknown;
-}
-
 class TestTagManager {
   private isDebugSessionActive: boolean;
   private config: vscode.WorkspaceConfiguration;
@@ -15,7 +10,6 @@ class TestTagManager {
   }
 
   private writeSettings(testTag: string | undefined): void {
-    console.log("Updating to: ", testTag);
     if (testTag === undefined) {
       this.config.update(
         "go.testTags",
@@ -35,15 +29,12 @@ class TestTagManager {
     const line = content.split("\n", 3)[0];
     if (line.includes("//go:build")) {
       if (line.includes("integration")) {
-        console.log("integration");
         return "integration";
       }
       if (line.includes("unit")) {
-        console.log("unit");
         return "unit";
       }
     }
-    console.log("default");
     return "unit";
   }
 
@@ -66,7 +57,6 @@ class TestTagManager {
 }
 
 export function activate(context: vscode.ExtensionContext): void {
-  console.log("activate tag switcher");
   const tagManager = new TestTagManager();
 
   const changeDisposable = vscode.window.onDidChangeActiveTextEditor(
